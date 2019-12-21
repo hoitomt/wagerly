0c4d6358227d5c3bf7faa9840a2f4a48d7d0aebe
Mike Hoitomt <mike@hoitomt.com>
2019-12-21 10:51:42 -0600

    Switch from secrets.yml to credentials.yml.enc

--- config/routes.rb
+++ config/routes.rb
@@ -17,5 +17,8 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
+  resources :users, only: [:show] do
+  end
+
   get 'todos' => 'pages#todos'
 end
